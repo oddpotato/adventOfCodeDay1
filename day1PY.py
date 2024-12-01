b66ab2e72f07f7b421bcc5fff9ccb8ac496c9bbe
Fin Apps <wikipanyd@gmail.com>
2024-12-01 11:57:39 +0000

    Finishing up day 1 with part 2!

--- day1PY.py
+++ day1PY.py
@@ -5,6 +5,7 @@ class DayOne:
     def __init__(self):
         self.day1List1 = []
         self.day1List2 = []
+        self.day1List3 = []
 
     def part1(self):
         with open('input.csv', 'r') as file:
@@ -16,6 +17,16 @@ class DayOne:
         self.day1List1.sort()
         self.day1List2.sort()
         result = list(map(lambda x, y: abs(x - y), self.day1List1, self.day1List2))
+        print("Should be: 3508942 and is: "+ str(sum(result)))
         print(sum(result))
+        return sum(result)
+    
+    def part2(self):
+        for num in self.day1List1:
+            self.day1List3.append(num * self.day1List2.count(num))
+        print(sum(self.day1List3))
+        return sum(self.day1List3)
 
-newDay = DayOne().part1()
+newDay = DayOne()
+newDay.part1()
+newDay.part2()
